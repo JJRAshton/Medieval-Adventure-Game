ae4d570156f75da7f0885933b4fa27d6b3852f11
JJRAshton <jjrashton@gmail.com>
2022-11-06 19:08:11 +0000

    backend entity

---
+++ online/dnd/entity.py
@@ -0,0 +1,17 @@
+
+
+class Entity:
+    def __init__(self):
+        self.health = 0
+        self.movement = 0
+        
+        self.str = 0
+        self.dex = 0
+        self.con = 0
+        self.int = 0
+        self.wis = 0
+        self.char = 0
+        
+        self.weapon = None
+        
+        self.
