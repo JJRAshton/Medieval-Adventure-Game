a021533f1f3e0f400fe898ce162ab762efb09657
JJRAshton <jjrashton@gmail.com>
2022-11-07 20:50:43 +0000

    split entity into animates and objects

--- online/dnd/animates.py
+++ online/dnd/animates.py
@@ -1,65 +1,6 @@
-import random as rd
+from objects import Entity
 
-class Entity:
-    def __init__(self):
-        self.baseSize = 5
-        self.weight = 0
-        self.weightTotal = 0
-        
-        self.baseHealth = 0
-        self.health = 0
-        
-        self.inventory = []
-        self.alive = True
-        
-        self.coords = (0,0)
-        
-        self.getStats()
-        self.resetSize()
-        self.calcWeight()
-    
-    #Moves to new coords           
-    def move(self, vector):
-        self.coords = (self.coords[0]+vector[0],self.coords[1]+vector[1])
-            
-    def resetHealth(self):
-        self.health = self.baseHealth
-    
-    def resetSize(self):
-        self.size = self.baseSize
-    
-    #Checks if entity is still alive
-    def checkHealth(self):
-        if self.health < 0:
-            self.alive = False
-            self.health = 0
-            
-    #Damages the entity
-    def takeDamage(self, damage):
-        (number,dice,bonus) = damage
-        base = 0
-        for _ in range(number):
-            base += rd.randint(1,dice)
-        appliedDamage = base + bonus
-        self.health -= appliedDamage
-        
-        self.checkHealth()
-        
-        return appliedDamage
-        
-    #Collects entity base stats
-    def getStats(self): # yet to get from jamie
-        self.baseSize
-        self.weight
-        self.baseHealth
-    
-    #Calculates the entity weight
-    def calcWeight(self):
-        self.weightTotal = self.weight
-        for item in self.inventory:
-            self.weightTotal += item.weightTotal
-        
-class MovingEntity(Entity):
+class AnimateEntity(Entity):
     def __init__(self):
         super().__init__(self)
         self.lvl = 0
@@ -203,6 +144,7 @@ class MovingEntity(Entity):
         self.primaryWeapon = 
         self.mod['STR'] = 
         self.profBonus = 
+        self.maxMovement = 
         
     #Collects entity base stats
     def getStats(self): # yet to get from jamie
@@ -214,65 +156,13 @@ class MovingEntity(Entity):
         self.armour
         self.primaryWeapon
         
-class Object(Entity):
-    def __init__(self):
-        super().__init__(self)
-        
-class Weapon(Object):
-    def __init__(self):
-        super().__init__(self)
-        self.damage = (0,0)
-        self.reach = 5
-        
-        self.ranged = False
-        
-        self.loading = False
-        self.arrows = False
-        self.bolts = False
-        self.light = False
-        self.heavy = False
-        self.versitile = False
-        self.finesse = False
-    
-    #Collects entity base stats
-    def getStats(self): # yet to get from jamie
-        super().getStats(self)
-        self.damage
-        self.reach
-        
-        self.ranged
-        
-        self.loading
-        self.arrows
-        self.bolts
-        self.light
-        self.heavy
-        self.finesse
-        
-    #Collects entity end stats
-    def overrideStats(self): # temporary measure to work with just these stats
-        self.damage = 
-        self.reach = 
-        
-class Armour(Object):
-    def __init__(self):
-        super().__init__(self)
-        self.type = ''
-        self.armourValue = 0
-    
-    #Collects entity base stats
-    def getStats(self): # yet to get from jamie
-        super().getStats(self)
-        self.type
-        self.armourValue
-
 #A playable character        
-class Player(MovingEntity):
+class Player(AnimateEntity):
     def __init__(self):
         super().__init__(self)
 
 #A non-playable character        
-class NPC(MovingEntity):
+class NPC(AnimateEntity):
     def __init__(self):
         super().__init__(self)
         self.target = None
