264bca6e432f029e8e2245db32da3467ebe7e9cd
JJRAshton <jjrashton@gmail.com>
2022-11-08 20:56:22 +0000

    map1 pickled

--- online/src/backend/map.py
+++ online/src/backend/map.py
@@ -1,13 +1,15 @@
+import pickle as pkl
 
 class Map:
 	def __init__(self, mapNumber):
 		self.map = mapNumber
-		self.cells = []
+		self.grid = []
 
 		self.getMap()
 
 	#Loads in the map from the map number given
 	def getMap(self):
+		self.grid = pkl.load(open('maps/map'+str(self.map)+'.pkl','rb'))
 
 	#Gets what is in cell at given coords
 	def getCell(self, coords):
--- online/src/backend/map_generation.py
+++ online/src/backend/map_generation.py
@@ -1,6 +1,14 @@
-import
+import pickle as pkl
 
 def generateMap(number):
 	grid = [[0 for _ in range(10)] for _ in range(10)]
 
-	
+	grid[8][8] = 1
+	grid[9][3] = 1
+
+	fileName = 'maps/map' + str(number) + '.pkl'
+
+	return grid, fileName
+
+grid, fileName = generateMap(1)
+pkl.dump(grid, open(fileName, 'wb'))
