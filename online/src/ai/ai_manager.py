7fb1b5861820d3fdb3be80ef1d57184d338b3e6e
jamwoody <110626689+jamwoody@users.noreply.github.com>
2022-11-26 18:14:51 +0000

    first AI, needs connecting to backend

--- online/src/ai/ai_manager.py
+++ online/src/ai/ai_manager.py
@@ -1,4 +1,4 @@
-from .brain import Brain
+from .brain import Brain1
 class AIManager:
     # Gets prompted to make a move or decision by a backend endpoint,
     # then makes a call to the backend API.
@@ -6,14 +6,22 @@ class AIManager:
     def __init__(self, backend):
         # This is the access point to the backend
         self.brains = []
+        self.backend = backend
 
     def notify(self, turn_character_id, is_player):
         pass
 
-    def takeTurn(self, id):
-        # Called by the backend
-        raise NotImplementedError
-    
+# Sorry for putting backend everywhere, don't know what to do with it
+    def takeTurn(self, backend, id):
+        # Make a brain
+        ai_brain = Brain1(backend, id)
+        # Choose target for approaching/attacking
+        target = ai_brain.choose_target(backend)
+        # Run up to target and keep attacking if in range
+        ai_brain.approach_and_attack_target(backend, target)
+        # Turn has ended
+        backend.Requests.endTurnRequest()
+
     def checkReaction(self):
         # I think we've agreed not to do reactions for now, but this is where it would go
         raise NotImplementedError
--- online/src/ai/brain.py
+++ online/src/ai/brain.py
@@ -1,49 +1,92 @@
 import math
 
-# Hardcoded stuff
+# Hardcoded stuff for testing
 row = [0 for i in range(0,10)]
 map_grid = [row for j in range(0,10)]
 
 # locations is the result of a locationsRequest
 locations = {'1': (1, 1), '2': (2, 5), '3': (9, 10), '4': (1,2)}
 
-class Brain:
+class Brain1:
+
+    """
+    Brain1 is an AI with the following decision process:
+     - Turn begins
+     - Makes a dictionary of all character locations, starting with the nearest one
+     - Chooses the closest character of the opposing team as its target
+     - While the target is out of attack range, moves towards the target (prioritises diagonal movement)
+     - Attacks when the target is in range, using all of its actions to attack
+     - If movement runs out, ends turn without doing anything else
+     - If actions run out, ends turn without doing anything else
+
+    What Brain1 does NOT do:
+     - Move around terrain blocking its path (will end turn early instead)
+     - Choose a 'new target' if it kills the one picked at the start of its turn
+     - Kite if it has a ranged weapon
+     - Factor in anything other than 'closeness' when choosing a target
+     - Have an awareness of its own mortality
+     - Do anything other than move and attack
+    """
+
     def __init__(self, backend, id):
-        self.locations = backend.locationsRequest()
+        self.locations = backend.Requests.locationsRequest()
         self.my_id = id
-        self.my_location = backend.locationsRequest()[id]
-        self.is_player = False
-        #self.movement = backend.infoRequest(id)[Remaining_movement]
+        self.my_location = backend.Requests.locationsRequest()[id]
+        #self.my_range = backend.back_requests.infoRequest(id)[Range]
+        self.my_movement = backend.Requests.infoRequest(id)['Remaining_movement']
+        self.actions = backend.Requests.infoRequest(id)['Action_number']
+
+    def check_distance(self, char_id):
+        target_location = self.locations[char_id]
+        x_distance = self.my_location[0] - target_location[0]
+        y_distance = self.my_location[1] - target_location[1]
+        distance = math.sqrt(x_distance ** 2 + y_distance ** 2)
+
+        return distance
+
+    def check_team(self, char_id, backend):
+        team = backend.Requests.infoRequest(char_id)['Team'] # 0 is player, 1 is npc
+
+        return team
 
     def choose_target(self, backend):
         character_distances = {}
         for option in self.locations:
-            target_location = self.locations[option]
-            x_distance = self.my_location[0] - target_location[0]
-            y_distance = self.my_location[1] - target_location[1]
-            distance = math.sqrt(x_distance**2 + y_distance**2)
-            character_distances.update({option: distance})
+            option_distance = self.check_distance(option)
+            character_distances.update({option: option_distance})
 
         closest_chars = sorted(character_distances.items(), key=lambda item: item[1])
 
         # Go through the list to find the nearest character of the opposing team
         for char in closest_chars:
-            if backend.infoRequest(char)['Team'] == 0:
+            if self.check_team(char, backend) == 0:
                 target = char
                 print(f"chosen target is {char}")
                 break
 
         return target
 
+    def check_can_attack(self, target):
+        if self.range < self.check_distance(target):
+            print("target is in range!")
+            return True
+        return False
 
-        squares_away = self.movement_required(char)
-        if self.movement < squares_away:
-            pass
-
-
-    def movement_required(self, target_location):
-        squares = 2 # avoid error
-        print(f"target is in range by moving {squares} squares")
-        return squares
+# Temporary backend references below
+    def approach_and_attack_target(self, backend, target):
+        target_location = self.locations[target]
+        while self.actions > 0:
+            while not self.check_can_attack(self, target):
+                x_movement = (target_location[0]-self.my_location[0])/abs(target_location[0]-self.my_location[0])
+                y_movement = (target_location[1]-self.my_location[1])/abs(target_location[1]-self.my_location[1])
+                movement_coords = (self.my_location[0] + x_movement, self.my_location[1] + y_movement)
+                # I think this line will also make a move request, not just check if true?
+                if not backend.Requests.moveRequest(self.my_id, movement_coords):
+                    self.actions = 0    # Not really needed now
+                    print('ran out of movement')
+                    return
+            backend.Requests.attackRequest(self.my_id, target)
+            self.actions -= 1
+        # Ends turn after a move request is denied (i.e. out of movement) or using up all actions
 
 print('game')
