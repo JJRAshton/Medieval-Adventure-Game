ab055d81dfa057e95a68dec35c79f60792f10651
SamuelBanfield <55929837+SamuelBanfield@users.noreply.github.com>
2022-11-27 21:23:41 +0000

    Fix displayed enemy locations, show health bar

--- online/src/api/api_turn_notification_subscription.py
+++ online/src/api/api_turn_notification_subscription.py
@@ -8,13 +8,30 @@ class APITurnNotificationSubscription(TurnNotificationSubscription):
     def __init__(self, playerPool):
         super().__init__()
         self.socketPool = {user.socket for user in playerPool}
+        self.requests = None
 
     def notify(self, character_on_turn, is_player):
         websockets.broadcast(self.socketPool, json.dumps(
             {
                 "responseType": "turnNotification",
-                "onTurnID": character_on_turn
+                "onTurnID": character_on_turn,
+                "charactersUpdate": self.getCharactersUpdate()
             }
         ))
+    
+    def getCharactersUpdate(self):
+        if self.requests is None:
+            raise ValueError("Requests not initialised")
+        locations = self.requests.locationsRequest()
+        return {charcter_id: self.getUpdateForCharacter(charcter_id, locations[charcter_id]) for charcter_id in locations}
+
+    def getUpdateForCharacter(self, id, location):
+        characterInfo = self.requests.infoRequest(id)
+        return {
+            "Health": characterInfo["Health"],
+            "coords": location
+        }
+
+
 
 
